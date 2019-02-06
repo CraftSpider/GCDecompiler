fd0e4765f138a6a3908fc09f35a2339d60701ea5
Rune Tynan <runetynan@gmail.com>
2019-02-06 12:58:03 -0500

    Setup now working, updated .gitignore, CMakeLists no longer linking zlib

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ src_dir=$(pwd)
 
 function cmake_build {
     if [ -z "$3" ]; then
-        echo "Cloning branch $3 of project $2"
-        git clone --single-branch --branch $3 https://github.com/$1/$2 libs/$2;
-    else
         echo "Cloning project $2"
         git clone https://github.com/$1/$2 libs/$2;
+    else
+        echo "Cloning branch $3 of project $2"
+        git clone --single-branch --branch $3 https://github.com/$1/$2 libs/$2;
     fi
 
     cd libs/$2
@@ -20,28 +20,25 @@ function cmake_build {
 }
 
 function copy_headers {
-    rsync -avm --include='*.h' -f 'hide,! */' libs/$1 libs/$2
+    rsync -avm --include='*.h' --include='*.tpp' --include='at_*' -f 'hide,! */' libs/$1/ libs/$2/
 }
 
 function copy_lib {
-    cp $1/*.a libs/
+    cp libs/$1/*.a libs/
 }
 
-mkdir libs
+function cmake_dep {
+    mkdir libs/$2
 
-# Clone and build zlib
-author=madler
-project=zlib
+    cmake_build $1 $2
+    copy_headers $2 $3
+    copy_lib $2
 
-cmake_build "madler" "zlib"
+    rm -rf libs/$2
+}
 
-# Copy over .h files into libs/include and .a into libs
-copy_headers "include" "zlib"
-copy_lib "zlib"
+mkdir -p libs/include
 
 
-cmake_build "craftspider" "alphatools" "alphalib"
-
-# Copy over .h files into libs/include and .a into libs
-copy_headers "libs" "alphatools"
-copy_lib "alphatools"
+cmake_dep "madler" "zlib" "include"
+cmake_dep "craftspider" "alphatools"
